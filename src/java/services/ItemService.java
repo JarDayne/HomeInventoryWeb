0c9e0ae871006f97d60fe94518e169df3d051254
Jaren <Jaren@DESKTOP-RJVCJC9>
2022-04-22 15:23:47 -0600

    CPRG 352 Final Project Jarenz Oria
    -transaction methods partially implemented for Items, Category models

--- src/java/services/ItemService.java
+++ src/java/services/ItemService.java
@@ -5,10 +5,52 @@
  */
 package services;
 
+import dataaccess.CategoryDB;
+import dataaccess.ItemDB;
+import java.util.List;
+import models.Category;
+import models.Item;
+import models.User;
+
 /**
  *
  * @author Jaren
  */
 public class ItemService {
+    
+    //Get Items to display for Owner
+    public List<Item> getAllByOwner(User owner) { 
+    
+        ItemDB itemData = new ItemDB(); 
+        List<Item> ownedItems = itemData.getAllByOwner(owner); 
+        return ownedItems;
+    } 
+    
+    //Add item for Owner
+    public void addItem(Item newItem, User owner) { 
+    
+        System.out.println("hi");
+    } 
+            
+            
+    //Delete item for Owner 
+            
+            
+    //Edit item for Owner
+    public void updateItem(int itemID, int categoryID, String itemName, double price) { 
+        
+        ItemDB itemData = new ItemDB(); 
+        Item item = itemData.get(itemID); 
+        
+        CategoryDB categoryData = new CategoryDB();
+        Category category = categoryData.get(categoryID); 
+        
+        item.setCategory(category); 
+        item.setItemName(itemName); 
+        item.setPrice(price);
+        
+        itemData.update(item);
+    } 
+            
     
 }
